b223e14bb039e762233120b4cb9ee7a9d544a176
Maiss Dev <maissen.developer500@gmail.com>
2023-05-29 20:46:24 +0200

    fix save custom sounds issue.

--- index/main.js
+++ index/main.js
@@ -1,33 +1,35 @@
 function save_custom(){
-    var item = document.getElementById('custom_item').cloneNode(true);
-    item.style.display = 'flex';
-    var customized = document.getElementById('customized');
-    x = prompt('What would you like as a name ?');
-    item.getElementsByClassName('name')[0].textContent = x;
-    item.id = '';
+    if(count != 0){
+        var item = document.getElementById('custom_item').cloneNode(true);
+        item.style.display = 'flex';
+        var customized = document.getElementById('customized');
+        x = prompt('What would you like as a name ?');
+        item.getElementsByClassName('name')[0].textContent = x;
+        item.id = '';
 
-    var items = document.querySelectorAll('.sounds-gallery .item');
-    
+        var items = document.querySelectorAll('.sounds-gallery .item');
+        
 
-    for(let o = 0; o < items.length; o++){
-        if(items[o].children[0].classList.contains('activeSound')){
-            item.children[2].classList += o + '-' + items[o].children[2].value + '/';
-        }
-    }
-
-    customized.appendChild(item);
-    var saved_items = document.getElementsByClassName('custom_item');
-    for(let j = 0; j < saved_items.length; j++){
-        saved_items[j].onclick = function(event){
-            if(event.target != "<button type=\"button\"><i class=\"fa-solid fa-heart\"></i></button>"){
-                play_custom(saved_items[j].children[2].getAttribute('class'));
+        for(let o = 0; o < items.length; o++){
+            if(items[o].children[0].classList.contains('activeSound')){
+                item.children[2].classList += o + '-' + items[o].children[2].value + '/';
             }
         }
-    }
-    for(let j = 0; j < saved_items.length; j++){
-        saved_items[j].children[1].children[1].onclick = function(){
-            if(confirm("Are you sure you want to delete " + item.getElementsByClassName('name')[0].textContent)){
-                customized.removeChild(saved_items[j]);
+
+        customized.appendChild(item);
+        var saved_items = document.getElementsByClassName('custom_item');
+        for(let j = 0; j < saved_items.length; j++){
+            saved_items[j].onclick = function(event){
+                if(event.target != "<button type=\"button\"><i class=\"fa-solid fa-heart\"></i></button>"){
+                    play_custom(saved_items[j].children[2].getAttribute('class'));
+                }
+            }
+        }
+        for(let j = 0; j < saved_items.length; j++){
+            saved_items[j].children[1].children[1].onclick = function(){
+                if(confirm("Are you sure you want to delete " + item.getElementsByClassName('name')[0].textContent)){
+                    customized.removeChild(saved_items[j]);
+                }
             }
         }
     }
